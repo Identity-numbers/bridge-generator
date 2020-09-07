0aa68d637a5f221e4457b8e5ce55e98eb7e01348
Linus Jarbo <linus@linusjarbo.com>
2020-09-07 18:26:47 +0200

    Initial stub
    to do list added

---
+++ bridge/Assets/Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    /*
+    GENETIC ALGORITHM APPROACH
+
+    define two numbers, one in and one out
+    two textfields
+
+    output inline
+
+    take first number and cut up in pieces, ordered, could be user defined or random like genetic code
+
+    set up first population
+    caclulate a fittnessvalue
+
+    breed at a certain threshold,
+    use similar and mate, use non-similar in beginning
+
+    give respons if the solution is exakt or not.
+    
+    able to add a placehold number and get a new approch number
+
+    being able to save each bridge
+
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
