2f5c24e493f5c16918f4859e767e0f8e1d36191c
Linus Jarbo <linus@linusjarbo.com>
2020-09-09 16:56:57 +0200

    End of day, I'll break here for now

--- bridge/Assets/Scripts/Main.cs
+++ bridge/Assets/Scripts/Main.cs
@@ -11,24 +11,20 @@ public class Main : MonoBehaviour
     public InputField endNumber;
     public InputField OutputTextField;
     public InputField populationSize;
+    public InputField generationSize;
+
+    private gaIndividualCollection[] population;
 
     /*
     GENETIC ALGORITHM APPROACH
 
-    take first number and cut up in pieces, ordered, could be user defined or random like genetic code
-
-    set up first population
     caclulate a fittnessvalue
-
     breed at a certain threshold,
     use similar and mate, use non-similar in beginning
-
     give respons if the solution is exakt or not.
-    
+    if individual have exact solution, add to solutions?
     able to add a placehold number and get a new approch number
-
     being able to save each bridge
-
     */
 
     void Start()
@@ -36,6 +32,8 @@ public class Main : MonoBehaviour
         //set default numbers and texts
         startNumber.text = "543210";
         endNumber.text = "102343";
+        populationSize.text = "20";
+        generationSize.text = "200";
         clearOutputWindow("Output window");
     }
 
@@ -45,7 +43,22 @@ public class Main : MonoBehaviour
         clearOutputWindow();
 
         //create initial population
+        createPopulation();
+
+        //evaluate individual and their fitnessvalue
+
+        //top half breed new population
+    }
+    
+    private void createPopulation()
+    {
+        //initiate population
+
+        //take first number and cut up in pieces, ordered, could be user defined or random like genetic code
+
+        //bread population with individuals
         //get startnumber and chop up in pieces
+
     }
 
     private void clearOutputWindow(string str = "")
--- bridge/Assets/Scripts/gaIndividual.cs
+++ bridge/Assets/Scripts/gaIndividual.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class gaIndividual : MonoBehaviour
 {
+    public int usedNumber;
+    public int usedNumberLenght;
+    public int fromIndex;
+    public int toIndex;
     //chopped number from original number
     public int choppedNumber;
     //negative or positive 1, or other?
