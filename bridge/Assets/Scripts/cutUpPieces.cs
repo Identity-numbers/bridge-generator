96226511ff9c2a55904db9f2ac9b118a4b717183
Linus Jarbo <linus@linusjarbo.com>
2020-09-11 10:13:41 +0200

    Added cut up pieces class

---
+++ bridge/Assets/Scripts/cutUpPieces.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutUpPieces : MonoBehaviour
+{
+    //start number to cut up
+    public int startNumber;
+
+    List<int> cutUpP = new List<int>();
+
+    //return a random piece from collection
+    public int returnRandomPiece()
+    {
+        int r = Random.Range (0, cutUpP.Count);
+        return r;
+    }
+
+    public void cutItUp()
+    {
+        if (startNumber == null)
+        {
+            Debug.Log("startNumber not set");
+        }
+
+        int tempNmbr = startNumber;
+
+        while (tempNmbr >0)
+        {
+            cutUpP.Add(tempNmbr);
+            tempNmbr /= 10; 
+        }
+    }
+}
